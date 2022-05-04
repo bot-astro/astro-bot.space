17be317756cec8adb193c65e99f82f479685bbc3
Oskar Lang <oskar.risto.lang@gmail.com>
2022-05-04 13:56:23 +0200

    feat: bootstrap tailwind and global fonts

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+      },
+      fontFamily: {
+        'display': ['Evolve', ...defaultTheme.fontFamily.sans],
+        'sans': ['Poppins', ...defaultTheme.fontFamily.sans]
+      }
+    }
+  }
+}
