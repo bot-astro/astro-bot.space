0d5213fa24c9bfdcb13cdbce48cd602670433b12
Oskar Lang <oskar.risto.lang@gmail.com>
2024-07-10 09:11:42 +0200

    feat: implement guild errors section

---
+++ components/modal/ModalError.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex flex-col gap-2 p-2">
+    <p>
+      {{ data.description }}
+    </p>
+    <p class="text-sm opacity-50">
+      {{ data.instant }}
+    </p>
+    <Button @click="modal.dismiss()">
+      Dismiss
+    </Button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import type { components } from '#nuxt-api-party/api'
+
+defineProps<{
+  data: components['schemas']['GuildError']
+}>()
+
+const modal = useModalStore()
+</script>
