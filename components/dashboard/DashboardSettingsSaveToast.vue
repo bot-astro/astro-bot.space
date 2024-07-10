71009d52dcfe1f0f1a566e798f2317ff2a867e65
Oskar Lang <oskar.risto.lang@gmail.com>
2024-07-10 08:33:28 +0200

    feat: integrate errors and simplify global dashboard handling

--- components/dashboard/DashboardSettingsSaveToast.vue
+++ components/dashboard/DashboardSettingsSaveToast.vue
@@ -1,9 +1,9 @@
 <template>
   <Transition>
-    <div v-show="settingsEdited" class="min-w-full fixed flex justify-center bottom-0 z-30 p-4">
+    <div v-show="edited" class="min-w-full fixed flex justify-center bottom-0 z-30 p-4">
       <div class="group h-min flex items-center justify-between text-sm shadow-xl w-full md:w-1/2">
         <span>You have unsaved changes</span>
-        <Button @click="dashboard.saveSettings()" :loading="settingsLoading">
+        <Button @click="dashboard.save()" :loading="loading">
           Save changes
         </Button>
       </div>
@@ -14,5 +14,5 @@
 <script lang="ts" setup>
 const dashboard = useDashboardStore()
 
-const { settingsEdited, settingsLoading } = storeToRefs(dashboard)
+const { edited, loading } = storeToRefs(dashboard)
 </script>
--- stores/dashboard.ts
+++ stores/dashboard.ts
@@ -2,17 +2,30 @@ import { deepEqual } from 'fast-equals'
 
 import type { components } from '#nuxt-api-party/api'
 
+interface GuildErrorWrapper {
+  guild_id: string
+  errors: components['schemas']['GuildError'][]
+}
+
 export const useDashboardStore = defineStore('dashboard', () => {
+  // const currentGuildId = computed(() => useRoute().params.guildId as string)
   const currentGuildId = ref(useRoute().params.guildId as string)
 
   const guilds = ref<components['schemas']['DiscordGuild'][]>([])
   const currentGuild = computed(() => guilds.value.find(g => g.id === currentGuildId.value))
 
   if (import.meta.dev) {
-    import('~/guilds.json').then(r => guilds.value = r.guilds)
+    import('~/guilds.json').then(r => guilds.value = r.default.guilds)
   } else {
     $api('/dashboard/guilds').then(r => guilds.value = r)
   }
+
+  const loading = ref(false)
+  const edited = ref(false)
+
+  /**
+   * Settings
+   */
 
   const settings = ref<components['schemas']['GuildData'][]>([])
 
@@ -20,7 +33,7 @@ export const useDashboardStore = defineStore('dashboard', () => {
     if (!currentGuildId.value) {
       return null
     }
-    
+
     const _settings = settings.value.find(s => s.guild_id === currentGuildId.value)
     if (!_settings) {
       const r = await $api('/dashboard/guilds/{guild_id}/data', {
@@ -43,19 +56,18 @@ export const useDashboardStore = defineStore('dashboard', () => {
     }
   }, { deep: true })
 
-  const settingsEdited = ref(false)
   watch(currentSettingsMutable, async (newSettings) => {
     const oldSettings = await currentSettings.value
-    settingsEdited.value = !deepEqual(newSettings, oldSettings)
+    edited.value = !deepEqual(newSettings, oldSettings)
   }, { deep: true })
-  const settingsLoading = ref(false)
 
   const saveSettings = async () => {
-    if (!settingsEdited) {
+    if (!edited) {
       return
     }
 
-    settingsLoading.value = true
+    loading.value = true
+
     const r = await $api('/dashboard/guilds/{guild_id}/data', {
       path: {
         guild_id: currentGuildId.value
@@ -65,10 +77,177 @@ export const useDashboardStore = defineStore('dashboard', () => {
         allow_missing_admin_perm: currentSettingsMutable.value?.allow_missing_admin_perm
       }
     })
-    settingsLoading.value = false
-
     settings.value.splice(settings.value.findIndex(s => s.guild_id === r.guild_id), 1, r)
+
+    loading.value = false
   }
+
+  /**
+   * Errors
+   */
+
+  const errors = ref<GuildErrorWrapper[]>([])
+
+  const currentErrors = computed(async () => {
+    if (!currentGuildId.value) {
+      return null
+    }
+
+    const wrapper = errors.value.find(w => w.guild_id === currentGuildId.value)
+    if (!wrapper) {
+      const r = await $api('/dashboard/guilds/{guild_id}/errors', {
+        path: {
+          guild_id: currentGuildId.value
+        }
+      })
+      // errors.value = [...errors.value, { guild_id: currentGuildId.value, errors: r }]
+      errors.value = [...errors.value, { guild_id: currentGuildId.value, errors: [{ description: "Your server has exceeded the maximum amount of interfaces.\nEither upgrade to premium or delete one or more interface with `/interface delete`!", instant: new Date().toLocaleString() }] }]
+      console.log(errors.value)
+      // return r
+      return errors.value.find(w => w.guild_id === currentGuildId.value)?.errors
+    } else {
+      return wrapper?.errors
+    }
+  })
+
+  const clearErrors = async () => {
+    loading.value = true
+
+    await $api('/dashboard/guilds/{guild_id}/errors', {
+      path: {
+        guild_id: currentGuildId.value
+      },
+      method: 'DELETE'
+    })
+
+    loading.value = false
+  }
+
+  /**
+   * Generators
+   */
+
+  const currentGenerators = computed(async () => {
+    const settings = await currentSettings.value
+
+    if (!settings) {
+      return null
+    }
+
+    return settings.generators
+  })
+
+  const currentGeneratorId = ref(useRoute().params.generatorId as string)
+  // const currentGeneratorId = computed(() => useRoute().params.generatorId as string)
+
+  const currentGenerator = computed(async () => {
+    const generators = await currentGenerators.value
+
+    if (!generators) {
+      return null
+    }
+
+    return generators.find(g => g.id === currentGeneratorId.value)
+  })
+
+  const currentGeneratorMutable = ref<components['schemas']['GeneratorData'] | null>(null)
+  watch(currentGenerator, async (newGenerator) => {
+    const result = await newGenerator
+    if (result) {
+      currentGeneratorMutable.value = useClone(result)
+    }
+  })
+
+  watch(currentGeneratorMutable, async (newGenerator) => {
+    const oldGenerator = await currentGenerator.value
+    edited.value = !deepEqual(newGenerator, oldGenerator)
+  })
+
+  // CREATE
+  const createGenerator = async () => {
+    if (!currentGeneratorMutable.value) {
+      return
+    }
+
+    loading.value = true
+
+    const r = await $api('/dashboard/guilds/{guild_id}/data/generator', {
+      path: {
+        guild_id: currentGuildId.value
+      },
+      method: 'POST'
+    })
+    settings.value.splice(settings.value.findIndex(s => s.guild_id === r.guild_id), 1, r)
+
+    loading.value = false
+  }
+
+  // MUTATE
+  const saveGenerator = async () => {
+    if (!edited.value || !currentGeneratorMutable.value) {
+      return
+    }
+
+    loading.value = true
+
+    const r = await $api('/dashboard/guilds/{guild_id}/data/generator/{generator_id}', {
+      path: {
+        guild_id: currentGuildId.value,
+        generator_id: currentGeneratorId.value
+      },
+      method: 'POST',
+      body: currentGeneratorMutable.value,
+    })
+    settings.value.splice(settings.value.findIndex(s => s.guild_id === r.guild_id), 1, r)
+
+    loading.value = false
+  }
+
+  // DELETE
+  const deleteGenerator = async () => {
+    if (!currentGenerator.value) {
+      return
+    }
+
+    loading.value = true
+
+    await $api('/dashboard/guilds/{guild_id}/data/generator/{generator_id}', {
+      path: {
+        guild_id: currentGuildId.value,
+        generator_id: currentGeneratorId.value
+      },
+      method: 'DELETE',
+    })
+
+    loading.value = false
+  }
+
+  // important: refresh guild settings after interacting with backend (separate function?)
+  // versus: update local data optimistacally?
+
+  /**
+   * Saving
+   */
+
+  const save = () => {
+    if (!edited.value) {
+      return
+    }
+
+    switch (currentSection.value.name) {
+      case 'Overview':
+        saveSettings()
+        break
+      case 'Generators':
+        saveGenerator()
+      default:
+        return
+    }
+  }
+
+  /**
+   * Sections
+   */
 
   const sections = ref(useRouter()
     .getRoutes()
@@ -86,28 +265,42 @@ export const useDashboardStore = defineStore('dashboard', () => {
       currentSection.value = section
     }
 
-    if (guildId) {
-      currentGuildId.value = guildId
-    }
-
     navigateTo(currentSection.value.path.replace(':guildId()', guildId ?? currentGuildId.value))
   }
 
   return {
-    currentGuildId,
-
+    // gleneral
     guilds,
+    currentGuildId,
     currentGuild,
 
+    edited,
+    loading,
+
+    // settings
     settings,
     currentSettings,
     currentSettingsMutable,
 
-    settingsEdited,
-    settingsLoading,
-
     saveSettings,
 
+    // errors
+    currentErrors,
+
+    clearErrors,
+
+    // generators
+    currentGenerators,
+    currentGeneratorMutable,
+
+    createGenerator,
+    saveGenerator,
+    deleteGenerator,
+
+    // saving
+    save,
+
+    // sections
     sections,
     currentSection,
 
